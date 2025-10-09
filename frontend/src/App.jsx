03f027b914c2d5df68c838e2056d4e814296ff01
harrist03 <harristeh85@gmail.com>
2025-10-09 17:48:17 +0100

    initial landing page + everything you need section done, needs minor fixes on desktop view for the background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from 'react'
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetchData();
-  },[])
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const res = await fetch("http://localhost:5000/api/test");
-      const data = await res.json();
-      setMessage(data.message);
-    } catch (err) { 
-      setError(err);
-      console.error("Error fetching data.. ", err);
-    } finally {
-      setLoading(false);
-    }
-  }
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      <p>{message}</p>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+      </Routes>
+    </>
   )
 }
 
