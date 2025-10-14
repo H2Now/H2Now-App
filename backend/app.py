dbeb3bfbecf6fee9b5fe25aeba8a9fb4756457f3
harrist03 <harristeh85@gmail.com>
2025-10-14 23:17:06 +0100

    created register functionality + basic errpr handling

--- backend/app.py
+++ backend/app.py
@@ -32,67 +32,85 @@ def hello():
     return jsonify({"message": "Hello World"})
 
 
-# For rendering index.html 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# Register endpoint. Creates user and hashes password
+@app.route("/api/auth/register", methods=["POST"])
+def register():
+    data = request.get_json(silent=True)
 
+    name = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
 
-# Create an initial test user 
-def create_test_user():
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    # error handling
+    errors = {}
+    if not name:
+        errors["username"] = "Username is required"
+    if not email:
+        errors["email"] = "Email is required"
+    elif "@" not in email:
+        errors["email"] = "Invalid email format"
+    if not password or len(password) < 8 or len(password) > 32:
+        errors["password"] = "Password must be 8-32 characters"
+    if errors:
+        print(errors)
+        # returns an array of error/s
+        return jsonify({"success": False, "errors": errors}), 400
+    
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-    name = "Test User"
-    email = "test@example.com"
-    password = TEST_PASSWORD.encode('utf-8') # Convert into bytes for hashing to work
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
-    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        cursor.execute(
+            "INSERT INTO User (name, email, password) VALUES(%s, %s, %s)",
+            (name, email, hashed_password.decode('utf-8'))
+        )
 
-    cursor.execute(
-        "INSERT INTO User (name, email, password) VALUES (%s, %s, %s)",
-        (name, email, hashed_password.decode('utf-8'))  # Decode password beforehand
-    )
+        conn.commit()
+    finally:
+        cursor.close()
+        conn.close()
 
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# create_test_user() # Comment if test user already created
-
+    return jsonify({"success": True, "message": "Registration successful! Please login.."}), 201
 
 # Login endpoint. Verifies email and password hash
 @app.route("/api/auth/login", methods=["POST"])
 def login():
     data = request.get_json(silent=True)
+
     email = data.get("email")
     password = data.get("password").encode("utf-8")
 
+    # error handling
     if not email or not password:
-        return jsonify({"error": "Email and password required"}), 400
+        return jsonify({"success": False, "message": "Invalid login details"})
+    
+    try: 
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True) # Return map/dictionary instead of tuple
 
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True) # Return map/dictionary instead of tuple
+        cursor.execute(
+            "SELECT * FROM User WHERE email=%s", 
+            [email]
+        )
 
-    cursor.execute(
-        "SELECT * FROM User WHERE email=%s", 
-        [email]
-    )
+        user = cursor.fetchone()
+    finally:
+        cursor.close()
+        conn.close()
 
-    user = cursor.fetchone()
-    cursor.close()
-    conn.close()
+    # if user is not found, return immediately
+    if not user:
+        return jsonify({"success": False, "message": "Invalid login details"})
 
     hashed_password = user["password"].encode('utf-8')
 
     # Compare hashes and continue if valid
     if user and bcrypt.checkpw(password, hashed_password):
-        print("Valid")
-        return jsonify({"message": "Login successfully", "user": user})
-    
-    print("Invalid")
-    return jsonify({"message": "Invalid login details"}), 401
+        return jsonify({"success": True, "message": "Login successfully"}), 201
+
+    return jsonify({"success": False, "message": "Invalid login details"}), 400
 
 
 if __name__ == "__main__":
