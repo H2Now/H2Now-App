fb9e2d9db6c9a26c45a35323d797518c482aba39
harrist03 <harristeh85@gmail.com>
2025-10-14 23:17:06 +0100

    created register functionality + basic errpr handling

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 function LoginPage() {
+    // TBC: display a modal containing data.message
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
@@ -8,46 +9,59 @@ function LoginPage() {
 
     const handleLoginSubmit = async (e) => {
         e.preventDefault();
+        setError(null)
+        setLoading(true);
         try {
             const res = await fetch("http://localhost:5000/api/auth/login", {
                 method: "POST",
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({ email, password}),
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }),
             })
-            if (!res.ok) throw new Error("Login failed");
             const data = await res.json();
 
-            console.log("Logged in: ", data.user);
-            console.log(data.message);
+            if (!data.success) {
+                setError(data.message);
+            } else {
+                console.log("Login successfully!")
+            }
         } catch (error) {
-            setError(error.message);
+            alert("Something went wrong. Please try again.", error);
         } finally {
             setLoading(false);
         }
     }
     return (
         <div>
-            {error && <p>{error}</p>}
+            {error && <p className="error">{error}</p>}
             <form onSubmit={handleLoginSubmit}>
-                <input
-                    type="email"
-                    name="email"
-                    autoComplete="off"
-                    placeholder="Enter you email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    autoComplete="off"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit" disabled={loading}>Submit</button>
+                <div>
+                    <label htmlFor="email">Email</label><br />
+                    <input
+                        className="w-[400px] border-1 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        id="email"
+                        type="email"
+                        name="email"
+                        autoComplete="off"
+                        placeholder="Enter you email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label><br />
+                    <input
+                        className="w-[400px] border-1 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        type="password"
+                        autoComplete="off"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button className="border" type="submit" disabled={loading}>{loading ? "Logging in..." : "Login"}</button>
             </form>
         </div>
     )
