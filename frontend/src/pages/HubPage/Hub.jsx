4bf56cb8d3d2b744c39d04f634ca3a3bfabf3acd
Oleksandr <o.oleksandr.m@gmail.com>
2025-10-14 16:42:22 +0100

    Feat: Implemented header and navbar for phones

--- frontend/src/pages/HubPage/Hub.jsx
+++ frontend/src/pages/HubPage/Hub.jsx
@@ -1,7 +1,14 @@
-import MyBottle from "./MyBottle"
+import Header from "../../components/Header"
+import MobileNavbar from "../../components/MobileNavbar"
 
 export default function Hub() {
     return (
-        <MyBottle />
+        <>
+            <Header text="H2Now" />
+
+            <div className="h-[calc(100vh-200px)]"></div>
+
+            <MobileNavbar />
+        </>
     )
 }
