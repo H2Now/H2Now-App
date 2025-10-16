d079f3788e69641b8e00a4ff280c145f6ba4217b
Oleksandr <o.oleksandr.m@gmail.com>
2025-10-16 11:32:19 +0100

    Fix: Implemented responsiveness

--- frontend/src/components/MobileNavbar.jsx
+++ frontend/src/components/MobileNavbar.jsx
@@ -5,17 +5,17 @@ import Settings from "../assets/icons/settings.png"
 export default function MobileNavbar() {
     return (
         <div className="w-full h-[100px] bg-[#AFAFAF] rounded-t-[15px] flex items-center justify-evenly block sm:hidden">
-            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer">
-                <img src={Account} className="w-8 h-8" />
-                <p className="text-[12px] text-[#161616]">Account</p>
+            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer [@media_(min-width:200px)_(max-width:299px)]:w-[50px] [@media(max-width:199px)]:w-[40px]">
+                <img src={Account} className="w-8 h-8 [@media(max-width:199px)]:w-6 [@media(max-width:199px)]:h-6" />
+                <p className="text-[12px] text-[#161616] [@media(max-width:199px)]:text-[10px]">Account</p>
             </div>
-            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer">
-                <img src={Bottle} className="w-8 h-8" />
-                <p className="text-[12px] text-[#161616]">My Bottle</p>
+            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer [@media_(min-width:200px)_(max-width:299px)]:w-[50px] [@media(max-width:199px)]:w-[40px]">
+                <img src={Bottle} className="w-8 h-8 [@media(max-width:199px)]:w-6 [@media(max-width:199px)]:h-6" />
+                <p className="text-[12px] text-[#161616] [@media_(min-width:200px)_(max-width:299px)]:text-[10px] [@media(max-width:199px)]:text-[8.5px]">My Bottle</p>
             </div>
-            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer">
-                <img src={Settings} className="w-8 h-8" />
-                <p className="text-[12px] text-[#161616]">Settings</p>
+            <div className="w-[75px] h-[75px] flex flex-col items-center justify-evenly cursor-pointer [@media_(min-width:200px)_(max-width:299px)]:w-[50px] [@media(max-width:199px)]:w-[40px]">
+                <img src={Settings} className="w-8 h-8 [@media(max-width:199px)]:w-6 [@media(max-width:199px)]:h-6" />
+                <p className="text-[12px] text-[#161616] [@media(max-width:199px)]:text-[10px]">Settings</p>
             </div>
         </div>
     )
