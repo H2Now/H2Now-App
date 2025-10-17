3fb6d4d5c609aeb6bfd18bdec8a7fb9d84b86ee6
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-10-17 09:25:28 +0100

    Test: Completed more tests for login
    Item: #61523

--- backend/tests/test_login.py
+++ backend/tests/test_login.py
@@ -15,14 +15,14 @@ def client():
         yield client
 
 
-# Test that checks to see if logic functionality is correct
+# Test that checks to see if valid user can login correctly 
 def test_login_valid_user(client, monkeypatch):
     def mock_get_db_connection():
         # Simulate and match procedure of db_connection in app.py
         class MockCursor:
             def cursor(self, dictionary=True): return self
             def execute(self, query, params): pass
-            def fetchone(self): return {"email": "user@test.com", "password": bcrypt.hashpw(b"password123", bcrypt.gensalt()).decode("utf-8")}
+            def fetchone(self): return {"email": "user@test.com", "password": bcrypt.hashpw("password123".encode('utf-8'), bcrypt.gensalt()).decode("utf-8")}
             def close(self): pass
 
         class MockConn:
@@ -31,8 +31,8 @@ def test_login_valid_user(client, monkeypatch):
 
         return MockConn()
 
-    import app
     # Replace original db_connection temporarily with mock_db_connection when called
+    import app
     monkeypatch.setattr(app, "get_db_connection", mock_get_db_connection)
 
     response = client.post("/api/auth/login", json={
@@ -40,6 +40,131 @@ def test_login_valid_user(client, monkeypatch):
         "password": "password123"
     })
 
-    # Must return 201 and a JSON with "success" == true
+    # Pass test if following are matched
     assert response.status_code == 201
     assert response.json["success"] == True
+    assert response.json["message"] == "Login successfully"
+
+
+# Test to make sure invalid user cannot login
+def test_login_invalid_user(client, monkeypatch):
+    def mock_get_db_connection():
+        # Simulate and match procedure of db_connection in app.py
+        class MockCursor:
+            def cursor(self, dictionary=True): return self
+            def execute(self, query, params): pass
+            def fetchone(self): None # Return nothing to simulate no user found
+            def close(self): pass
+
+        class MockConn:
+            def cursor(self, dictionary=True): return MockCursor()
+            def close(self): pass
+
+        return MockConn()
+    
+    # Replace original db_connection temporarily with mock_db_connection when called
+    import app
+    monkeypatch.setattr(app, "get_db_connection", mock_get_db_connection)
+
+    response = client.post("/api/auth/login", json={
+        "email": "user@test.com",
+        "password": "password123"
+    })
+
+    # Pass test if following are matched
+    assert response.status_code == 400
+    assert response.json["success"] == False
+    assert response.json["message"] == "Invalid login details"
+
+
+# Test to make sure forms with any unfilled credentials do not pass
+def test_login_missing_credentials(client, monkeypatch):
+    def mock_get_db_connection():
+        # Simulate and match procedure of db_connection in app.py
+        class MockCursor:
+            def cursor(self, dictionary=True): return self
+            def execute(self, query, params): pass
+            def fetchone(self): return {"email": "user@test.com", "password": bcrypt.hashpw("password123".encode('utf-8'), bcrypt.gensalt()).decode("utf-8")}
+            def close(self): pass
+
+        class MockConn:
+            def cursor(self, dictionary=True): return MockCursor()
+            def close(self): pass
+
+        return MockConn()
+    
+    # Replace original db_connection temporarily with mock_db_connection when called
+    import app
+    monkeypatch.setattr(app, "get_db_connection", mock_get_db_connection)
+
+    response = client.post("/api/auth/login", json={
+        "email": "",
+        "password": ""
+    })
+
+    # Pass test if following are matched
+    assert response.status_code == 400
+    assert response.json["success"] == False
+    assert response.json["message"] == "Please fill out all fields!"
+
+
+# Test to make sure forms with no given email do not pass
+def test_login_missing_email(client, monkeypatch):
+    def mock_get_db_connection():
+        # Simulate and match procedure of db_connection in app.py
+        class MockCursor:
+            def cursor(self, dictionary=True): return self
+            def execute(self, query, params): pass
+            def fetchone(self): return {"email": "user@test.com", "password": bcrypt.hashpw("password123".encode('utf-8'), bcrypt.gensalt()).decode("utf-8")}
+            def close(self): pass
+
+        class MockConn:
+            def cursor(self, dictionary=True): return MockCursor()
+            def close(self): pass
+
+        return MockConn()
+    
+    # Replace original db_connection temporarily with mock_db_connection when called
+    import app
+    monkeypatch.setattr(app, "get_db_connection", mock_get_db_connection)
+
+    response = client.post("/api/auth/login", json={
+        "email": "",
+        "password": "password123"
+    })
+
+    # Pass test if following are matched
+    assert response.status_code == 400
+    assert response.json["success"] == False
+    assert response.json["message"] == "Please fill out all fields!"
+
+
+# Test to make sure forms with no given password do not pass
+def test_login_missing_password(client, monkeypatch):
+    def mock_get_db_connection():
+        # Simulate and match procedure of db_connection in app.py
+        class MockCursor:
+            def cursor(self, dictionary=True): return self
+            def execute(self, query, params): pass
+            def fetchone(self): return {"email": "user@test.com", "password": bcrypt.hashpw("password123".encode('utf-8'), bcrypt.gensalt()).decode("utf-8")}
+            def close(self): pass
+
+        class MockConn:
+            def cursor(self, dictionary=True): return MockCursor()
+            def close(self): pass
+
+        return MockConn()
+    
+    # Replace original db_connection temporarily with mock_db_connection when called
+    import app
+    monkeypatch.setattr(app, "get_db_connection", mock_get_db_connection)
+
+    response = client.post("/api/auth/login", json={
+        "email": "user@test.com",
+        "password": ""
+    })
+
+    # Pass test if following are matched
+    assert response.status_code == 400
+    assert response.json["success"] == False
+    assert response.json["message"] == "Please fill out all fields!"
