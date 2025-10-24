3ba623f239013b848ebf47167929602daf08793b
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-10-24 17:52:45 +0100

    refractor: modify bottle and intake table to reflect 1:1 (previously 1:M)  between user-bottle

--- db/schema.sql
+++ db/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE User (
 -- ==============================
 CREATE TABLE Bottle (
     bottleID INT AUTO_INCREMENT PRIMARY KEY,
-    userID INT NOT NULL,
+    userID INT UNIQUE NOT NULL,
     bottleName VARCHAR(150) NOT NULL,
     capacity DECIMAL(6,2) NOT NULL,
     goal DECIMAL(6,2) NOT NULL,
@@ -38,14 +38,13 @@ CREATE TABLE Bottle (
 -- ==============================
 CREATE TABLE Intake (
     intakeID INT AUTO_INCREMENT PRIMARY KEY,
-    userID INT NOT NULL,
     bottleID INT NOT NULL,
     intakeDate DATE NOT NULL,
     totalIntake DECIMAL(6,2) NOT NULL,
     goalReached BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
     FOREIGN KEY (bottleID) REFERENCES Bottle(bottleID) ON DELETE CASCADE
 );
+
 
 -- ==============================
 -- USER PREFERENCES TABLE
