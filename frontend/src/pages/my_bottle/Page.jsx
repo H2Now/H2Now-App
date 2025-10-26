57dc6fae448dd71c9da269faf1bc92865d44d23b
Oleksandr <o.oleksandr.m@gmail.com>
2025-10-26 02:18:17 +0000

    Style: Modified mobile navbar for My Bottle page

--- frontend/src/pages/my_bottle/Page.jsx
+++ frontend/src/pages/my_bottle/Page.jsx
@@ -11,7 +11,7 @@ export default function Page() {
                 <UserBottle />
             </div>
 
-            <MobileNavbar />
+            <MobileNavbar page="My Bottle" />
         </>
     )
 }
