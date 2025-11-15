e7a44401bb1d9201edfa64c5e9fa5d5da2161928
theo-picar1 <theopicar@icloud.com>
2025-11-15 12:47:01 +0000

    Fix: Clean and fix inaccuracies

--- backend/hardware/test_bottle.py
+++ backend/hardware/test_bottle.py
@@ -1,6 +1,9 @@
 # For PI only to run
 from bottle import BottleHardware
 import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
 monitor = BottleHardware()
 monitor.start()
